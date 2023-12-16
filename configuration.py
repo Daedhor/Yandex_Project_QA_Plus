43c782d24db087a752dd1d4f4aee079039df8969
Daedhor <khaylov94@mail.ru>
2023-12-16 10:59:52 +0300

    Update BASE_URL

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-BASE_URL = "https://77478e5b-7c2e-46d9-bdf3-9f96434d230f.serverhub.praktikum-services.ru"
+BASE_URL = "https://21971990-72e5-4722-a5fe-130617b6410b.serverhub.praktikum-services.ru"
 CREATE_USER_PATH = "/api/v1/users/"
 CREATE_KITS_PATH = "/api/v1/kits"
 USER_TOKEN = ""
